c43c0c4889ec60e96b712b4ef735f193a405d6e8
ZackLoken <zack@savannainstitute.org>
2024-06-27 13:18:19 -0500

    Update datasets.py

--- models_src/basemodel.pt/datasets.py
+++ models_src/basemodel.pt/datasets.py
@@ -7,60 +7,59 @@ import json, os
 import pandas as pd
 from collections import defaultdict
 
-
 def load_image(path:str) -> PIL.Image:
     '''Load image, rotate according to EXIF orientation'''
     image = PIL.Image.open(path).convert('RGB')
-    # image = PIL.ImageOps.exif_transpose(image)
+    image = PIL.ImageOps.exif_transpose(image)
     return image
 
 
-# read image name from jsonfile
-def get_imagename_from_jsonfile(jsonfile):
-    with open(jsonfile, 'r') as j:
-        jsondata = json.loads(j.read())
-    return jsondata['item']['slots'][0]['source_files'][0]['file_name']
+def guess_encoding(x:bytes) -> str:
+    try:
+        return x.decode('utf8')
+    except UnicodeDecodeError:
+        return x.decode('cp1250')
 
 
-def get_boxes_from_jsonfile(jsonfile):
-    '''Reads bounding boxes from a DARWIN json file and returns them as a (Nx4) array'''
-    with open(jsonfile, 'r') as j:
-        jsondata = json.loads(j.read())
-    boxes = []
-    for i in range(len(jsondata['annotations'])):
-        box = [jsondata['annotations'][i]['bounding_box']['x'], 
-                jsondata['annotations'][i]['bounding_box']['y'], 
-                jsondata['annotations'][i]['bounding_box']['x']+jsondata['annotations'][i]['bounding_box']['w'], 
-                jsondata['annotations'][i]['bounding_box']['y']+jsondata['annotations'][i]['bounding_box']['h']]
-        box = np.array(box, dtype=float)
-        box.reshape(-1,4)
-        boxes.append(box)
-    return boxes # return as (Nx4) array of bounding
+def read_json_until_imagedata(jsonfile):
+    '''LabelMe JSON are rather large because they contain the whole image additionally to the labels.
+       This function reads a jsonfile only up to the imagedata attribute (ignoring everything afterwards) to reduce the loading time.
+       Returns a valid JSON string'''
+    f = open(jsonfile, 'rb')
+    f.seek(0,2); n=f.tell(); f.seek(0,0)
+    buffer = b''
+    while b'imageData' not in buffer and len(buffer)<n:
+        data      = f.read(1024*16)
+        buffer   += data
+        if len(data)==0:
+            return buffer
+    buffer   = buffer[:buffer.index(b'imageData')]
+    buffer   = buffer[:buffer.rindex(b',')]
+    buffer   = buffer+b'}'
+    return guess_encoding(buffer)
+
+
+def get_boxes_from_jsonfile(jsonfile, flip_axes=False):
+    '''Reads bounding boxes from a LabeLMe json file and returns them as a (Nx4) array'''
+    jsondata = json.loads(read_json_until_imagedata(jsonfile))
+
+    boxes    = [shape['points'] for shape in jsondata['shapes']]
+    boxes    = [[min(box[0],box[2]),min(box[1],box[3]),
+                 max(box[0],box[2]),max(box[1],box[3])] for box in np.reshape(boxes, (-1,4))]
+    boxes    = np.array(boxes)
+    boxes    = boxes[:,[1,0,3,2]] if flip_axes else boxes
+    return boxes.reshape(-1,4)
 
 
 def get_labels_from_jsonfile(jsonfile):
-    '''Reads a list of labels in a DARWIN json file.'''
-    with open(jsonfile, 'r') as j:
-        jsondata = json.loads(j.read())
-    return [ a['name'] for a in jsondata['annotations'] ]
- 
-
-def get_imagesize_from_jsonfile(jsonfile):
-    with open(jsonfile, 'r') as j:
-        jsondata = json.loads(j.read())
-    return (jsondata['item']['slots'][0]['height'], jsondata['item']['slots'][0]['width'])
+    '''Reads a list of labels in a json LabelMe file.'''
+    return [ s['label'] for s in json.loads( read_json_until_imagedata(jsonfile) )['shapes'] ]
 
 
-def create_df_from_jsonfile(jsonfiles) -> pd.DataFrame:
-    """Stores image names, labels, and boxes in a dataframe from a dir of json"""
-    df = pd.DataFrame(columns = ['image_name', 'labels', 'boxes'])
-    for jsonfile in os.listdir(jsonfiles):
-        boxes = np.array(get_boxes_from_jsonfile(jsonfiles + jsonfile))
-        labels = list(get_labels_from_jsonfile(jsonfiles + jsonfile))
-        img_name = str(get_imagename_from_jsonfile(jsonfiles + jsonfile))
-        for i in range(len(labels)):
-            df = df.append({'image_name': img_name, 'labels': labels[i], 'boxes': boxes[i]}, ignore_index = True)
-    return df
+def get_imagename_from_jsonfile(jsonfile):
+    jsondata = json.loads(read_json_until_imagedata(jsonfile))
+    return str(jsondata['imagePath'])
+
 
 
 def create_dataloader(dataset, batch_size, shuffle:bool, num_workers=0):
@@ -72,46 +71,52 @@ def create_dataloader(dataset, batch_size, shuffle:bool, num_workers=0):
                                        pin_memory=True)
 
 
-class DetectionDataset(torch.utils.data.Dataset):
+
+#class Dataset(torch.utils.data.Dataset):  #inheriting gives errors on unpickling
+class Dataset:
+    def __init__(self, jpgfiles, jsonfiles):
+        self.jsonfiles = jsonfiles
+        self.jpgfiles  = jpgfiles
+    
+    def __len__(self):
+        return len(self.jpgfiles)
+    
+    def __getitem__(self, i):
+        image, target = self.get_item(i)
+        return image, target
+
+
+
+class DetectionDataset(Dataset):
     """Dataset Loader for Waterfowl Drone Imagery"""
     
-    def __init__(self, jpgfiles, jsonfiles, augment:bool):
+    def __init__(self, jpgfiles, jsonfiles, augment:bool, negative_classes:list):
+        super().__init__(jpgfiles, jsonfiles)
         self.augment   = augment
-        self.jsonfiles = jsonfiles
-        self.df        = create_df_from_jsonfile(jsonfiles)
-        self.jpgfiles  = jpgfiles
-        
+        self.negative_classes = negative_classes
+
 
     def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-        
-        jsonfile = os.listdir(self.jsonfiles)[idx]
-
-        # load image as torch.datapoints image
-        image_path = os.path.join(self.jpgfiles, self.df['image_name'][idx])
-
-        image    = load_image(image_path)
+        jsonfile = self.jsonfiles[idx]
+        jpgfile = self.jpgfiles[idx]
+        image    = load_image(jpgfile)
+        width, height = image.size
         image    = dp.Image(image)
 
-        # load boxes
-        boxes    = self.df[self.df['image_name'] == self.df['image_name'][idx]]['boxes'].values
-        boxes = [np.array(box, dtype = np.float32) for box in boxes]
-        boxes = torch.as_tensor(boxes, dtype = torch.float32)
-        
-        #duck species labels
-        labels   = self.df['labels'][self.df['image_name'] == self.df['image_name'][idx]]
+        boxes = get_boxes_from_jsonfile(jsonfile)
 
-        label_dict = {4.0: 'MALL', 1.0: 'AMCO', 3.0: 'GWTE', 6.0: 'NSHO', 2.0: 'GADW', 8.0: 'RNDU', 5.0: 'NOPI', 7.0: 'REDH'}
+        labels = get_labels_from_jsonfile(jsonfile)
+
+        label_dict = {4.0: 'MALL', 1.0: 'AMCO', 3.0: 'GWTE', 6.0: 'NSHO', 2.0: 'GADW', 8.0: 'RNDU', 5.0: 'NOPI', 7.0: 'REDH'} #FIXME: hardcoded
         
         labels = [0 if label not in label_dict.values() else list(label_dict.keys())[list(label_dict.values()).index(label)] for label in labels]
 
+        boxes    = [box for box,label in zip(boxes, labels) if label not in self.negative_classes]
+        boxes    = np.array(boxes).reshape(-1,4)
+
         target = {}
-        target['boxes'] = dp.BoundingBox(boxes, format="XYXY", spatial_size=get_imagesize_from_jsonfile(self.jsonfiles + jsonfile))
+        target['boxes'] = dp.BoundingBox(boxes, format="XYXY", spatial_size=(height, width))
         target['labels'] = torch.as_tensor(labels, dtype=torch.int64)
-        target['image_id'] = torch.tensor([idx])
-        target['area'] = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-        target['iscrowd'] = torch.zeros((len(labels),), dtype=torch.int64)
 
         augments = []
         if self.augment is not None:
@@ -131,10 +136,6 @@ class DetectionDataset(torch.utils.data.Dataset):
         image, target = augments(image, target)
 
         return image, target
-
-
-    def __len__(self):
-        return len(self.df['image_name'].unique())
 
 
     @staticmethod
