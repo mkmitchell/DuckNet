ed6f4e0e422fe7768da2832a03f599eb36d91a89
ZackLoken <zack@savannainstitute.org>
2024-10-14 15:51:14 -0500

    update default lr

--- backend/training.py
+++ backend/training.py
@@ -33,7 +33,7 @@ def start_training(imagefiles, targetfiles, training_options:dict, settings):
                 num_workers       = 0, 
                 callback          = cb,
                 epochs            = training_options.get('epochs', 10),
-                lr                = training_options.get('learning_rate', 0.0005),
+                lr                = training_options.get('learning_rate', 0.005),
             )
 
         return 'OK' if ok else 'INTERRUPTED'
