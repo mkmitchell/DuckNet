118feb3bbecf9bbdf1d41893cd401a43df19f7a9
ZackLoken <zack@savannainstitute.org>
2025-05-17 14:46:36 -0500

    Update training.js

--- base/frontend/base/training.js
+++ base/frontend/base/training.js
@@ -16,24 +16,66 @@ BaseTraining = class BaseTraining{
         this.update_model_info()
     }
 
+    // static async on_start_training(){
+    //     var filenames = this.get_selected_files()
+    //     console.log('Training on ', filenames)
+        
+    //     const progress_cb = (m => this.on_training_progress(m))
+    //     try {
+    //         this.show_modal()
+    //         await this.upload_training_data(filenames)
+
+    //         $(GLOBAL.event_source).on('training', progress_cb)
+    //         //FIXME: success/fail should not be determined by this request
+    //         await $.post('/training', JSON.stringify({filenames:filenames, options:this.get_training_options()}))
+    //         if(!$('#training-modal .ui.progress').progress('is complete'))
+    //             this.interrupted_modal()
+            
+    //         GLOBAL.App.Settings.load_settings()
+    //     } catch (e) {
+    //         console.error(e)
+    //         this.fail_modal()
+    //     } finally {
+    //         $(GLOBAL.event_source).off('training', progress_cb)
+    //     }
+    // }
+
     static async on_start_training(){
+        console.log('Starting training process');
         var filenames = this.get_selected_files()
-        console.log('Training on ', filenames)
+        console.log('Training files ', filenames)
         
         const progress_cb = (m => this.on_training_progress(m))
         try {
             this.show_modal()
-            await this.upload_training_data(filenames)
-
+            
+            // Store the result of upload_training_data and log it
+            const uploadResult = await this.upload_training_data(filenames)
+            console.log('Upload result:', uploadResult)
+    
             $(GLOBAL.event_source).on('training', progress_cb)
+            // Log the training options
+            const trainingOptions = this.get_training_options();
+            console.log('Training options:', trainingOptions);
+    
+            // Log the data being sent to the backend
+            const requestData = { filenames: filenames, options: trainingOptions };
+            console.log('Request data:', requestData);
+    
             //FIXME: success/fail should not be determined by this request
-            await $.post('/training', JSON.stringify({filenames:filenames, options:this.get_training_options()}))
-            if(!$('#training-modal .ui.progress').progress('is complete'))
+            const response = await $.post('/training', JSON.stringify(requestData))
+            console.log('Training response:', response)
+    
+            if(!$('#training-modal .ui.progress').progress('is complete')) {
+                console.log('Training not complete, showing interrupted modal')
                 this.interrupted_modal()
+            } else {
+                console.log('Training complete')
+            }
             
             GLOBAL.App.Settings.load_settings()
         } catch (e) {
-            console.error(e)
+            console.error('Training failed:', e)
             this.fail_modal()
         } finally {
             $(GLOBAL.event_source).off('training', progress_cb)
@@ -110,8 +152,19 @@ BaseTraining = class BaseTraining{
         $('#training-modal #cancel-training-button').hide()
     }
 
+    // static on_training_progress(message){
+    //     var data = JSON.parse(message.originalEvent.data)
+    //     $('#training-modal .progress').progress({percent:data.progress*100, autoSuccess:false})
+    //     $('#training-modal .label').text(data.description)
+    //     if(data.progress >= 1){
+    //         this.success_modal()
+    //         //this.update_model_info()
+    //     }
+    // }
+
     static on_training_progress(message){
         var data = JSON.parse(message.originalEvent.data)
+        console.log('Progress update:', data)
         $('#training-modal .progress').progress({percent:data.progress*100, autoSuccess:false})
         $('#training-modal .label').text(data.description)
         if(data.progress >= 1){
@@ -129,18 +182,47 @@ BaseTraining = class BaseTraining{
         $('#training-new-modelname')[0].value = ''
     }
 
-    static upload_training_data(filenames){
+//     static upload_training_data(filenames){
+//         //TODO: show progress
+//         var promises      = filenames.map( f => upload_file_to_flask(GLOBAL.files[f]) )
+//         //TODO: refactor
+//         //TODO: standardize file name
+//         var segmentations = filenames.map(    f => GLOBAL.files[f].results.segmentation )
+//                                      .filter( s => s instanceof Blob )
+//         promises          = promises.concat( segmentations.map( f => upload_file_to_flask(f) ) )
+//         return Promise.all(promises).catch( this.fail_modal )  //FIXME: dont catch, handle in calling function
+//     }
+// }
+
+    static async upload_training_data(filenames){
+        // Log the filenames
+        console.log('Filenames:', filenames);
+
         //TODO: show progress
-        var promises      = filenames.map( f => upload_file_to_flask(GLOBAL.files[f]) )
+        var promises = filenames.map(f => {
+            console.log('Uploading file:', f);
+            return upload_file_to_flask(GLOBAL.files[f]);
+        });
+
         //TODO: refactor
         //TODO: standardize file name
-        var segmentations = filenames.map(    f => GLOBAL.files[f].results.segmentation )
-                                     .filter( s => s instanceof Blob )
-        promises          = promises.concat( segmentations.map( f => upload_file_to_flask(f) ) )
-        return Promise.all(promises).catch( this.fail_modal )  //FIXME: dont catch, handle in calling function
+        var segmentations = filenames.map(f => {
+            const segmentation = GLOBAL.files[f].results.segmentation;
+            console.log('Segmentation for file', f, ':', segmentation);
+            return segmentation;
+        }).filter(s => s instanceof Blob);
+
+        // Log the segmentations
+        console.log('Segmentations:', segmentations);
+
+        promises = promises.concat(segmentations.map(f => upload_file_to_flask(f)));
+        try {
+            return await Promise.all(promises);
+        } catch (result) {
+            return this.fail_modal(result);
+        }  //FIXME: dont catch, handle in calling function
     }
 }
-
 
 
 ObjectDetectionTraining = class extends BaseTraining {
@@ -154,131 +236,104 @@ ObjectDetectionTraining = class extends BaseTraining {
 
         const callback = (_ => this.refresh_class_selection())
         $('#classes-of-interest-dropdown').dropdown({onChange: callback})
-        $('#other-classes-dropdown').dropdown(      {onChange: callback})
-        $('#unknown-classes-dropdown').dropdown(    {onChange: callback})
+        $('#other-classes-dropdown').dropdown({onChange: callback})
+        $('#unknown-classes-dropdown').dropdown({onChange: callback})
         this.refresh_class_selection()
     }
 
     //override
     static upload_training_data(filenames){
         //TODO: show progress
-        const files           = filenames.map( k => GLOBAL.files[k] )
-        const targetfiles     = files.map(
+        const files = filenames.map(k => GLOBAL.files[k])
+        const targetfiles = files.map(
             f => GLOBAL.App.Download.build_annotation_jsonfile(f.name, f.results)
         )
 
-        const promises = files.concat(targetfiles).map( f => upload_file_to_flask(f) )
-        return Promise.all(promises).catch( this.fail_modal )
+        const promises = files.concat(targetfiles).map(f => upload_file_to_flask(f))
+        return Promise.all(promises).catch(this.fail_modal)
     }
 
     //override
     static get_training_options(){
-        const [coi_selected, oth_selected, unk_selected, rejected] = this.get_class_selection()
+        const [coi_selected, rejected] = this.get_class_selection()
 
         return {
-            classes_of_interest : coi_selected,
-            classes_other       : oth_selected,
-            classes_unknown     : unk_selected,
-            classes_rejected    : rejected,
-            train_detector      : $('#train-detector-checkbox').checkbox('is checked'),
-            learning_rate       : Number($('#training-learning-rate')[0].value),
-            epochs              : Number($('#training-number-of-epochs')[0].value),
+            classes_of_interest: coi_selected,
+            classes_rejected: rejected,
+            train_detector: $('#train-detector-checkbox').checkbox('is checked'),
+            learning_rate: Number($('#training-learning-rate')[0].value),
+            epochs: Number($('#training-number-of-epochs')[0].value),
         };
     }
 
-
     static collect_class_counts(){
         const filenames = this.get_selected_files()
-        const labels    = filenames
-                          .map( f => GLOBAL.files[f].results?.labels )
-                          .filter(Boolean)
-                          .flat()
-                          .map( l => l.trim() || GLOBAL.App.NEGATIVE_CLASS )
-        let   label_set = new Set(labels)
+        const labels = filenames
+            .map(f => GLOBAL.files[f].results?.labels)
+            .filter(Boolean)
+            .flat()
+            .map(l => l.trim() || GLOBAL.App.NEGATIVE_CLASS)
+        let label_set = new Set(labels)
         const known_classes = GLOBAL.App.Settings.get_properties_of_active_model()?.['known_classes'] ?? []
         known_classes.map(c => label_set.add(c))
-        const uniques   = [...(label_set)].sort()
-        const counts    = uniques.map( l => labels.filter( x => x==l ).length )
+        const uniques = [...(label_set)].sort()
+        const counts = uniques.map(l => labels.filter(x => x == l).length)
         return [uniques, counts]
     }
 
     static get_class_selection(){
-        const all_classes  = this.collect_class_counts()[0]
+        const all_classes = this.collect_class_counts()[0]
         const coi_selected = $('#classes-of-interest-dropdown').dropdown('get value').split(',')
-        const oth_selected = $('#other-classes-dropdown').dropdown('get value').split(',')
-        const unk_selected = $('#unknown-classes-dropdown').dropdown('get value').split(',')
-        const any_selected = [...coi_selected, ...oth_selected, ...unk_selected]
-        const rejected     = all_classes.filter( s => !any_selected.includes(s) )
+        const rejected = all_classes.filter(s => !coi_selected.includes(s))
 
-        return [coi_selected, oth_selected, unk_selected, rejected]
+        return [coi_selected, rejected]
     }
 
     static refresh_class_count_table(){
-        const [classes, counts]  = this.collect_class_counts()
+        const [classes, counts] = this.collect_class_counts()
 
-        const [coi_selected, oth_selected, unk_selected, _rejected] = this.get_class_selection()
-        const coi_ixs      = Object.keys(classes).filter( i => coi_selected.includes(classes[i]) )
-        const oth_ixs      = Object.keys(classes).filter( i => oth_selected.includes(classes[i]) )
-        const unk_ixs      = Object.keys(classes).filter( i => unk_selected.includes(classes[i]) )
+        const [coi_selected, rejected] = this.get_class_selection()
+        const coi_ixs = Object.keys(classes).filter(i => coi_selected.includes(classes[i]))
 
-        const $table       = $('table#detector-classes tbody')
+        const $table = $('table#detector-classes tbody')
         $table.html('')
         for(const i of coi_ixs){
-            //console.log(classes[i], counts[i])
             $(`<tr>
                 <td>${classes[i]}</td>
-                <td>${counts[i] }</td>
+                <td>${counts[i]}</td>
             </tr>`).appendTo($table)
         }
-        const coi_count    = coi_ixs.map( i => counts[i] ).reduce( (a,b) => a+b, 0 )
-        const oth_count    = oth_ixs.map( i => counts[i] ).reduce( (a,b) => a+b, 0 )
-        const unk_count    = unk_ixs.map( i => counts[i] ).reduce( (a,b) => a+b, 0 )
-        const rej_count    = counts.reduce( (a,b) => a+b, 0 ) - coi_count - oth_count - unk_count;
+        const coi_count = coi_ixs.map(i => counts[i]).reduce((a, b) => a + b, 0)
+        const rej_count = counts.reduce((a, b) => a + b, 0) - coi_count
         $('#classes-of-interest-count').text(coi_count)
-        $('#other-classes-count').text(oth_count)
-        $('#unknown-classes-count').text(unk_count)
         $('#rejected-classes-count').text(rej_count)
 
-        $('#detector-positive-classes-count').text(coi_count+oth_count+unk_count)
+        $('#detector-positive-classes-count').text(coi_count)
         $('#detector-negative-classes-count').text(rej_count)
     }
 
     static refresh_class_selection() {
-        const [coi_selected, oth_selected, unk_selected, rejected] = this.get_class_selection()
+        const [coi_selected, rejected] = this.get_class_selection()
         
-        const $coi_list     = $('#classes-of-interest-list')
+        const $coi_list = $('#classes-of-interest-list')
         $coi_list.html('')
-        rejected.map( s => $(`<div class="item" data-value="${s}">${s}</div>`).appendTo($coi_list) )
-
-        const $oth_list     = $('#other-classes-list')
-        $oth_list.html('')
-        rejected.map( s => $(`<div class="item" data-value="${s}">${s}</div>`).appendTo($oth_list) )
-
-        const $unk_list     = $('#unknown-classes-list')
-        $unk_list.html('')
-        rejected.map( s => $(`<div class="item" data-value="${s}">${s}</div>`).appendTo($unk_list) )
-
+        rejected.map(s => $(`<div class="item" data-value="${s}">${s}</div>`).appendTo($coi_list))
 
         const $rejectedlist = $('#rejected-classes-list')
         $rejectedlist.html('')
-        rejected.map( s => $(`<div class="ui label">${s}</div>`).appendTo($rejectedlist) )
+        rejected.map(s => $(`<div class="ui label">${s}</div>`).appendTo($rejectedlist))
 
         this.refresh_class_count_table()
     }
 
     static reset_class_selection(){
-        const all_classes     = this.collect_class_counts()[0]
-        let   known_classes   = GLOBAL.App.Settings.get_properties_of_active_model()?.['known_classes'] ?? []
-              known_classes   = known_classes.filter( x => x != GLOBAL.App.NEGATIVE_CLASS )
-        let   unknown_classes = all_classes.filter( x => x.includes('unknown') )
-                                           .filter( x => x.includes('undeterminable') )
+        const all_classes = this.collect_class_counts()[0]
+        let known_classes = GLOBAL.App.Settings.get_properties_of_active_model()?.['known_classes'] ?? []
+        known_classes = known_classes.filter(x => x != GLOBAL.App.NEGATIVE_CLASS)
 
         $('#classes-of-interest-dropdown')
             .dropdown('refresh')
             .dropdown('set selected', known_classes)
-        $('#unknown-classes-dropdown')
-            .dropdown('refresh')
-            .dropdown('set selected', unknown_classes)
     }
 }
 
