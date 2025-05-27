c6598fb1207b56086bf762df890425378b9e85fb
ZackLoken <zack@savannainstitute.org>
2025-05-27 00:11:23 -0500

    Update processing.py

--- backend/processing.py
+++ backend/processing.py
@@ -2,14 +2,12 @@ from base.backend import GLOBALS
 
 import exif
 
-
 #TODO: unify
 def process_image(imagepath, settings):
     with GLOBALS.processing_lock:
         model    = settings.models['detection']
         result   = model.process_image(imagepath)
     return result
-
 
 def load_exif_datetime(filename:str) -> str:
     with open(filename, 'rb') as f:
@@ -18,10 +16,11 @@ def load_exif_datetime(filename:str) -> str:
         except:
             #can fail for some weird exif formats
             print('Could not load exif')
-            return
+            return ''
         
         if exif_f.has_exif :
             if 'datetime_original' in exif_f.list_all():
                 return exif_f.datetime_original
             elif 'datetime' in exif_f.list_all():
                 return exif_f.datetime
+        return ''
