e9c23f6c5809ce1d075a703dbf3037ec9fe79408
ZackLoken <zack@savannainstitute.org>
2025-05-28 21:23:39 -0500

    .

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,15 @@
 from base.backend.app import App as BaseApp
 import backend.processing
 import backend.training
-
 import os
+import numpy as np
 import flask
 
 class App(BaseApp):
     def __init__(self, *a, **kw):
         super().__init__(*a, **kw)
         self.route('/read_exif_datetime')(self.read_exif_datetime)
+        self.route('/stop_training')(self.stop_training)
 
     #TODO: unify
     #override
@@ -21,29 +22,15 @@ class App(BaseApp):
         model = self.settings.models['detection']
         result = model.process_image(full_path)
         
-        threshold = self.settings.confidence_threshold/100
-        filtered_items = [(i, score) for i, score in enumerate(result['box_scores']) if score > threshold]
-        formatted_labels = [{result['labels'][i]: float(score)} for i, score in filtered_items]
-        
-        filtered_boxes = []
-        for i, _ in filtered_items:
-            box = result['boxes'][i]
-            if hasattr(box, 'tolist'):
-                filtered_boxes.append(box.tolist())
-            else:
-                filtered_boxes.append(list(box))
-        
         jsonresult = {
-            'labels': formatted_labels,
-            'boxes': filtered_boxes,
+            'labels': result['per_class_scores'],
+            'boxes': np.array(result['boxes']).tolist(),
             'datetime': backend.processing.load_exif_datetime(full_path)
         }
         
         print(jsonresult)
         return flask.jsonify(jsonresult)
-
-    #TODO: unify
-    #override
+    
     def training(self):
         requestform  = flask.request.get_json(force=True)
         options      = requestform['options']
@@ -52,9 +39,10 @@ class App(BaseApp):
         targetfiles  = backend.training.find_targetfiles(imagefiles)
         if not all(targetfiles):
             flask.abort(404)
+        
         ok = backend.training.start_training(imagefiles, targetfiles, options, self.settings)
         return ok
-    
+
     def read_exif_datetime(self):
         filename = flask.request.args['filename']
         full_path = os.path.join(self.cache_path, filename)
@@ -64,4 +52,3 @@ class App(BaseApp):
         return flask.jsonify({
             'datetime':  backend.processing.load_exif_datetime(full_path),
         })
-
